22792270137506b2ae256efbccc91f8d75f349c9
Zettervall <oskar.zettervall@gmail.com>
2012-11-23 11:52:17 +0100

    Update src/Participator.java
    lol

--- src/Participator.java
+++ src/Participator.java
@@ -1,60 +1,65 @@
-//package se.marathon.oskar;
-
 public class Participator
 {
-	private int age;
-	private int time;
-	private String name;
-	private String country;
-	private int startNumber;
+private int age;
+private double time;
+private String name;
+private String country;
+private int startNumber;
 
-	// Utan tid
-	public Participator(int age, String name, String country, int startNumber)
-	{
-		this.age = age;
-		this.name = name;
-		this.country = country;
-		this.startNumber = startNumber;
-	}
-	
-	// Med time
-	public Participator(int age, String name, int time, String country, int startNumber)
-	{
-		this.age = age;
-		this.name = name;
-		this.time = time;
-		this.country = country;
-		this.startNumber = startNumber;
-	}
-
-	public int getAge()
-	{
-		return age;
-	}
-	
-	public String getCountry()
-	{
-		return country;
-	}
-	
-	public String getName()
-	{
-		return name;
-	}
-	
-	public int getStartNumber()
-	{
-		return startNumber;
-	}
-	
-	public int getTime()
-	{
-		return time;
-	}
-	
-	public void setTime(int time)
-	{
-		this.time = time;
-	}
+// Utan tid
+public Participator(int age, String name, String country)
+{
+this.age = age;
+this.name = name;
+this.country = country;
 
 }
+
+// Med time
+//public Participator(int age, String name, double time, String country, int startNumber)
+//{
+//this.age = age;
+//this.name = name;
+//this.time = time;
+//this.country = country;
+//this.startNumber = startNumber;
+//}
+
+public Participator (double time){
+	this.time = time;
+}
+
+public int getAge()
+{
+return age;
+}
+
+public String getCountry()
+{
+return country;
+}
+
+public String getName()
+{
+return name;
+}
+
+public int getStartNumber()
+{
+return startNumber;
+}
+
+public double getTime()
+{
+return time;
+}
+
+public void setTime(int time)
+{
+this.time = time;
+}
+
+
+	
+}
+
