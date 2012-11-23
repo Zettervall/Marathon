0f395c2afd4dfb818832cb1769fe958bbfacb45e
Zettervall <oskar.zettervall@gmail.com>
2012-11-23 11:51:57 +0100

    Update src/Marathon.java
    lolokid

--- src/Marathon.java
+++ src/Marathon.java
@@ -1,132 +1,193 @@
-//package se.marathon.oskar;
-
+import static javax.swing.JOptionPane.*;
 import java.awt.*;
-import java.awt.event.*;
-
 import javax.swing.*;
+import java.awt.event.*;
+import java.util.*;
 
-public class Marathon extends JFrame implements ActionListener
+//import Marathon.NewRunnerForm;
+
+public class Marathon extends JFrame 
 {
 
-	public Marathon()
-	{
+private NewRunnerForm form;
 
-		setTitle("DSV Kista Maraton");
+public Marathon()
+{
 
-		Container pane = getContentPane();
-		pane.setLayout(new BorderLayout());
-		pane.add(getNorth(), BorderLayout.NORTH);
-		pane.add(getSouth(), BorderLayout.SOUTH);
-		pane.add(getEast(), BorderLayout.EAST);
-		pane.add(getWest(), BorderLayout.WEST);
+setTitle("DSV Kista Maraton");
 
-		int windowWidth = 400;
-		int windowHeight = 400;
-		setBounds(100, 100, windowWidth, windowHeight);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setVisible(true);
-	}
+Container pane = getContentPane();
+pane.setLayout(new BorderLayout());
+pane.add(getNorth(), BorderLayout.NORTH);
+pane.add(getSouth(), BorderLayout.SOUTH);
+pane.add(getEast(), BorderLayout.EAST);
+pane.add(getWest(), BorderLayout.WEST);
 
-	private JComponent getNorth()
-	{
+int windowWidth = 400;
+int windowHeight = 400;
+setBounds(100, 100, windowWidth, windowHeight);
+setDefaultCloseOperation(EXIT_ON_CLOSE);
+setVisible(true);
+}
 
-		JLabel label = new JLabel("DSV Kista Marathon", JLabel.CENTER);
-		return label;
-	}
+private JComponent getNorth()
+{
 
-	private JComponent getSouth()
-	{
+JLabel label = new JLabel("DSV Kista Marathon", JLabel.CENTER);
+return label;
+}
 
-		JPanel south = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		
-		JButton newButton = new JButton("New");
-		JButton showButton = new JButton("Show");
-		JButton timeButton = new JButton("Time");
-		
-		newButton.addActionListener(this);
-		showButton.addActionListener(this);
-		timeButton.addActionListener(this);
-		
-		south.add(newButton);
-		south.add(showButton);
-		south.add(timeButton);
-		
-		return south;
-	}
+private JComponent getSouth()
+{
 
-	private JComponent getWest()
-	{
+JPanel south = new JPanel(new FlowLayout(FlowLayout.LEFT));
 
-		JPanel west = new JPanel();
+Icon NewRunnerForm;
+JButton newButton = new JButton("New");
+JButton showButton = new JButton("Show");
+JButton timeButton = new JButton("Time");
 
-		JList txt = new JList();
-		Dimension size = new Dimension(270, 310);
-		JScrollPane scrolltxt = new JScrollPane(txt);
-		scrolltxt.setPreferredSize(size);
+newButton.addActionListener(new NewAction());
+//showButton.addActionListener(this);
+//timeButton.addActionListener(this);
 
-		west.add(scrolltxt);
-		return west;
+south.add(newButton);
+south.add(showButton);
+south.add(timeButton);
 
-	}
+return south;
+}
 
-	private JComponent getEast()
-	{
-		JPanel east = new JPanel();
-		east.setLayout(new BoxLayout(east, BoxLayout.Y_AXIS));
-		
-	
-		east.setAlignmentY(BOTTOM_ALIGNMENT);
+private JComponent getWest()
+{
 
-		
-		
-		
-		JLabel label = new JLabel("Sortering");
+JPanel west = new JPanel();
 
-		JRadioButton startButton = new JRadioButton("StartNumber");
-		startButton.setActionCommand("StartNumber");
-		startButton.setSelected(true);
+JList txt = new JList();
+Dimension size = new Dimension(270, 310);
+JScrollPane scrolltxt = new JScrollPane(txt);
+scrolltxt.setPreferredSize(size);
 
-		JRadioButton nameButton = new JRadioButton("Name");
-		nameButton.setActionCommand("Name");
-		
-		JRadioButton ageButton = new JRadioButton("Age");
-		ageButton.setActionCommand("Age");
-		
-		JRadioButton timeButton = new JRadioButton("Time");
-		timeButton.setActionCommand("Time");
+west.add(scrolltxt);
+return west;
 
-		// Register listeners
-		startButton.addActionListener(this);
-		nameButton.addActionListener(this);
-		ageButton.addActionListener(this);
-		timeButton.addActionListener(this);
+}
 
-		// Grupperar radioknapparna
-		ButtonGroup group = new ButtonGroup();
-		group.add(startButton);
-		group.add(nameButton);
-		group.add(ageButton);
-		group.add(timeButton);
+private JComponent getEast()
+{
+JPanel east = new JPanel();
+east.setLayout(new BoxLayout(east, BoxLayout.Y_AXIS));
 
-		// Adding buttons to JPanel
-		east.add(label);
-		east.add(startButton);
-		east.add(nameButton);
-		east.add(ageButton);
-		east.add(timeButton);
 
-		return east;
-	}
+east.setAlignmentY(BOTTOM_ALIGNMENT);
 
-	public void actionPerformed(ActionEvent e)
-	{
-		System.out.println(e.getActionCommand());
-	}
 
-	public static void main(String[] args)
-	{
-		new Marathon();
 
-	}
+
+JLabel label = new JLabel("Sortering");
+
+JRadioButton startButton = new JRadioButton("StartNumber");
+startButton.setActionCommand("StartNumber");
+startButton.setSelected(true);
+
+JRadioButton nameButton = new JRadioButton("Name");
+nameButton.setActionCommand("Name");
+
+JRadioButton ageButton = new JRadioButton("Age");
+ageButton.setActionCommand("Age");
+
+JRadioButton timeButton = new JRadioButton("Time");
+timeButton.setActionCommand("Time");
+
+// Register listeners
+//startButton.addActionListener(this);
+//nameButton.addActionListener(this);
+//ageButton.addActionListener(this);
+//timeButton.addActionListener(this);
+
+// Grupperar radioknapparna
+ButtonGroup group = new ButtonGroup();
+group.add(startButton);
+group.add(nameButton);
+group.add(ageButton);
+group.add(timeButton);
+
+// Adding buttons to JPanel
+east.add(label);
+east.add(startButton);
+east.add(nameButton);
+east.add(ageButton);
+east.add(timeButton);
+
+return east;
+}
+class NewRunnerForm extends JPanel{
+    private JTextField nameField, countryField, ageField;
+    public NewRunnerForm(){
+    setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+JPanel rad1 = new JPanel();
+rad1.add(new JLabel("Namn:"));
+nameField = new JTextField(10);
+rad1.add(nameField);
+add(rad1);
+        
+        JPanel rad2 = new JPanel();
+rad2.add(new JLabel("Land:"));
+countryField = new JTextField(10);
+rad2.add(countryField);
+add(rad2);
+        
+        JPanel rad3 = new JPanel();
+rad3.add(new JLabel("Ålder:"));
+ageField = new JTextField(10);
+rad3.add(ageField);
+add(rad3);
+    }
+    public String getName(){
+        return nameField.getText();
+    }
+    public String getCountry(){
+        return countryField.getText();
+    }
+    public int getAge(){
+        return Integer.parseInt(ageField.getText());
+    }
+} 
+class NewAction implements ActionListener{
+    public void actionPerformed(ActionEvent ave){
+       NewRunnerForm form = new NewRunnerForm();
+       
+       while(true){
+           try{
+           int svar = showConfirmDialog(Marathon.this, form);
+           if(svar != OK_OPTION)
+                return;
+           
+           String name = form.getName();
+           String country = form.getCountry();
+           int age = form.getAge();
+        
+           if(!name.equals("") && !country.equals("")){
+                Participator r = new Participator(age, name, country);
+//                Participator.add(r);
+                return;
+           }
+           else{
+             JOptionPane.showMessageDialog(Marathon.this, "Du måste fylla i alla fält");
+           }
+           }catch(NumberFormatException e){
+              JOptionPane.showMessageDialog(Marathon.this, "Du har fyllt i fel någonstans. Var god rätta till detta.");
+           }
+       }
+       
+    }
+    
+}
+
+public static void main(String[] args)
+{
+new Marathon();
+
+}
 
 }
